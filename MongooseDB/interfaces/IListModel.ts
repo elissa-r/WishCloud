16713c87aeb1d4690817dbd34b93a19ebbebc86b
Alivia <68788300+Aliviaaz@users.noreply.github.com>
2025-10-24 15:48:17 -0700

    Fix error with importing Mongoose

--- MongooseDB/interfaces/IListModel.ts
+++ MongooseDB/interfaces/IListModel.ts
@@ -1,6 +1,7 @@
-var Mongoose = require("mongoose");
+import type { Document } from "mongoose";
+const mongoose = require("mongoose");
 
-interface IListModel extends Mongoose.Document {
+interface IListModel extends Document {
     name: string;
     listId: number;
     userID: number;
@@ -8,4 +9,5 @@ interface IListModel extends Mongoose.Document {
     date: Date;
     budget: number;
 }
-export {IListModel};
+
+export { IListModel };
