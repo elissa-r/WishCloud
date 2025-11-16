adc6662c49c93d4efb71009d6336c05eaaf9fdd6
Elissa Ryan <elissalr@outlook.com>
2025-11-16 13:15:14 -0800

    start from scratch

--- src/wishcloud-client/src/app/models/wishlist.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Wishlist {
-  _id: string;        // MongoDB document id
-  name: string;
-  listId?: number;
-  userID: string;
-  photoLink?: string;
-  date?: string;      // ISO string from backend
-  budget?: number;
-}
