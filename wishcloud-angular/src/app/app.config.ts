80a744e1c61c14501da378d79e1df7789791b7e4
Alivia <68788300+Aliviaaz@users.noreply.github.com>
2025-11-16 17:10:15 -0800

    Do not merge this branch it's for testing purposes only

--- wishcloud-angular/src/app/app.config.ts
+++ wishcloud-angular/src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routing.module';
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule, HttpClientModule)
+    provideAnimations(),                     
+    importProvidersFrom(HttpClientModule)   
   ]
 };
+
