b44b1514c1fa127e1bfe500705ec909e9df0180c
Elissa Ryan <elissalr@outlook.com>
2025-12-02 21:27:59 -0800

    jest setup

---
+++ wishcloud-angular/jest.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['<rootDir>/src/app/**/*.spec.ts'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        useESM: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html', 'json'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@angular|rxjs|tslib/)', // allow Angular ESM to be transformed
+  ],
+};
