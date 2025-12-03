39940a186f64116370415f7684fe92648ec312cf
Elissa Ryan <elissalr@outlook.com>
2025-12-02 21:59:24 -0800

    Update setup-jest.ts

--- wishcloud-angular/setup-jest.ts
+++ wishcloud-angular/setup-jest.ts
@@ -4,3 +4,9 @@ setupZoneTestEnv();
 if (!(globalThis as any).fetch) {
   (globalThis as any).fetch = jest.fn();
 }
+
+if (!(globalThis as any).Response) {
+  (globalThis as any).Response = class {
+    constructor(..._args: any[]) {}
+  } as any;
+}
