e6b20196795fb7fe86fbf5d5a060093b1f1573a4
Shamitha Pichika <spichika@seattleu.edu>
2025-12-02 18:45:05 -0800

    fixed more stuff

--- wishcloud-angular/src/app/wishlistproxy.service.ts
+++ wishcloud-angular/src/app/wishlistproxy.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WishlistproxyService {
 
-  private hostUrl = 'http://localhost:3000/api';
+  private hostUrl = 'http://192.168.0.20:3000/api';
 
   constructor(private http: HttpClient) {}
 
